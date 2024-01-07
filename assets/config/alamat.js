d81611eda30e9a2fe80e6828abe85c0ecda195de
Daffa Pratama Nur Ardiansyah <106421588+daffapratamaaa@users.noreply.github.com>
2024-01-08 06:53:00 +0700

    Create alamat.js

---
+++ assets/config/alamat.js
@@ -0,0 +1,3 @@
+document.write('Desa Toyareja RT 02/RW 03 Kecamatan/Kabupaten Purbalingga<br>Jawa Tengah - Indonesia')
+//Sesuaikan, Jika ingin menggunakan karakter "enter" gunakan kode <br>
+//Jangan Menghapus kode scriptnya
